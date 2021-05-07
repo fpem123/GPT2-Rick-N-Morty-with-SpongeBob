6c9e2d98bba8d7ab422f82df3b4561264c8a21bc
LeeHoseop <ghtjq3909@naver.com>
2021-05-07 11:18:22 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,163 @@
+'''
+    Name: main.py
+    Writer: Hoseop Lee, Ainizer
+    Rule: Flask app server
+    update: 21.05.07
+'''
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from flask import Flask, request, jsonify, render_template
+import torch
+
+from queue import Queue, Empty
+from threading import Thread
+import time
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+app = Flask(__name__)
+
+# Model & Tokenizer loading
+tokenizer = AutoTokenizer.from_pretrained('ainize/gpt2-rnm-with-spongebob')
+model = AutoModelForCausalLM.from_pretrained('ainize/gpt2-rnm-with-spongebob')
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+
+requests_queue = Queue()    # request queue.
+BATCH_SIZE = 1              # max request size.
+CHECK_INTERVAL = 0.1
+
+
+##
+# Request handler.
+# GPU app can process only one request in one time.
+def handle_requests_by_batch():
+    while True:
+        request_batch = []
+
+        while not (len(request_batch) >= BATCH_SIZE):
+            try:
+                request_batch.append(requests_queue.get(timeout=CHECK_INTERVAL))
+            except Empty:
+                continue
+
+            for requests in request_batch:
+                try:
+                    requests["output"] = rnm_with_spongebob_script(requests['input'][0], requests['input'][1], requests['input'][2])
+                except:
+                    requests['output'] = 'Error occur in handler'
+
+
+Thread(target=handle_requests_by_batch).start()
+
+
+##
+# GPT-2 generator.
+# Make script.
+def rnm_with_spongebob_script(name, text, length):
+    try:
+        text = name + ': ' + text.strip()
+        input_ids = tokenizer.encode(text, return_tensors='pt')
+
+        # input_ids also need to apply gpu device!
+        input_ids = input_ids.to(device)
+
+        min_length = len(input_ids.tolist()[0])
+        length += min_length
+
+        length = length if length > 0 else 50
+
+        # model generating
+        sample_outputs = model.generate(input_ids, pad_token_id=50256,
+                                        do_sample=True,
+                                        max_length=length,
+                                        min_length=min_length,
+                                        top_k=40,
+                                        num_return_sequences=1)
+
+        result = dict()
+
+        for idx, sample_output in enumerate(sample_outputs):
+            story = tokenizer.decode(sample_output, skip_special_tokens=True).split('\n')
+
+            for i in range(len(story)):
+                if story[i]:
+                    if story[i][0] in ['(', '[']:
+                        story[i] = ['Narrator', story[i]]
+                    elif ':' in story[i]:
+                        story[i] = story[i].split(':')
+                    else:
+                        story[i] = [story[i - 1][0], story[i]]
+                else:
+                    continue
+
+            result[idx] = story
+
+        return result
+
+    except Exception as e:
+        print('Error occur in script generating!', e)
+        return jsonify({'error': e}), 500
+
+
+##
+# Get post request page.
+@app.route('/gen', methods=['POST'])
+def generate():
+    # GPU app can process only one request in one time.
+    if requests_queue.qsize() > BATCH_SIZE:
+        return jsonify({'Error': 'Too Many Requests'}), 429
+
+    try:
+        args = []
+
+        name = request.form['name']
+        text = request.form['text']
+        length = int(request.form['length'])
+
+        args.append(name)
+        args.append(text)
+        args.append(length)
+
+    except Exception as e:
+        return jsonify({'message': 'Invalid request'}), 500
+
+    # input a request on queue
+    req = {'input': args}
+    requests_queue.put(req)
+
+    # wait
+    while 'output' not in req:
+        time.sleep(CHECK_INTERVAL)
+
+    return jsonify(req['output'])
+
+
+##
+# Queue deadlock error debug page.
+@app.route('/queue_clear')
+def queue_clear():
+    while not requests_queue.empty():
+        requests_queue.get()
+
+    return "Clear", 200
+
+
+##
+# Sever health checking page.
+@app.route('/healthz', methods=["GET"])
+def health_check():
+    return "Health", 200
+
+
+##
+# Main page.
+@app.route('/')
+def main():
+    return render_template('main.html'), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
---
+++ static/IMG_Reference.md
@@ -0,0 +1,25 @@
+# Image reference
+
+Rick: http://vignette4.wikia.nocookie.net/rickandmorty/images/d/dd/Rick.png
+
+Morty: http://vignette3.wikia.nocookie.net/rickandmorty/images/c/ce/MortyTransparent.png
+
+Summer: http://vignette2.wikia.nocookie.net/rickandmorty/images/5/53/Summer_Wide-mouth_S1E11.JPG
+
+Beth: http://vignette4.wikia.nocookie.net/rickandmorty/images/5/58/Beth_Smith.png
+
+Jerry: http://vignette3.wikia.nocookie.net/rickandmorty/images/5/5d/Jerry_S01E11_Sad.JPG
+
+SpongeBob.png: http://cartoonbros.com/wp-content/uploads/2015/12/SpongeBob-SquarePants-1.png
+
+Patrick.png: https://upload.wikimedia.org/wikipedia/en/7/7e/Patrick_Star.png
+
+Squidward.png: http://vignette4.wikia.nocookie.net/mobrosstudios/images/0/0c/Squidward_Tentacles.png
+
+Mr. Krabs.png: http://vignette3.wikia.nocookie.net/spongefan/images/3/31/Mr._Krabs.jpg
+
+Plankton.png: http://vignette4.wikia.nocookie.net/villains/images/d/d3/Plankton_by_wombat7500-d417kmu.png
+
+Sandy.png: http://img4.wikia.nocookie.net/__cb20141107194531/advenutres-of-powerpuff-girls-z/images/6/6d/SandyCheeks.png
+
+Narrator: https://nickelodeon.fandom.com/wiki/French_Narrator?file=French_Narrator_%2528Frenchy%2529_waving.png
