99c65d5e5ef71c1d928d0c16c879b08a60d6adc9
LeeHoseop <ghtjq3909@naver.com>
2021-05-10 09:58:08 +0900

    Change port

--- main.py
+++ main.py
@@ -161,5 +161,5 @@ def main():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=80)
     #app.run(host='0.0.0.0', port=80)
